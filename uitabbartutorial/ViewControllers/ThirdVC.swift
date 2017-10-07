1c9dc337a8eda15209fb6aa57c5e56a9499db6ac
Alexey Adamovsky <adamovsky@Alexeys-MacBook-Pro.local>
2017-10-08 00:52:04 +0300

    Initial Commit

---
+++ uitabbartutorial/ViewControllers/ThirdVC.swift
@@ -0,0 +1,40 @@
+//
+//  ThirdVC.swift
+//  uitabbartutorial
+//
+//  Created by Alexey Adamovsky on 05/10/2017.
+//  Copyright © 2017 Alexey Adamovsky. All rights reserved.
+//
+
+import UIKit
+
+class ThirdVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        CustomTabBarController.shared?.customTabBar.setBadgeNum(num: 2)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
